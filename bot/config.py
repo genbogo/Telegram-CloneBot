e179a72b48e5cfe81eb2622e3173ce89b5923731
Genbogo <66338822+genbogo@users.noreply.github.com>
2021-04-27 02:11:03 +0000

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,10 +3,10 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = ""
-GDRIVE_FOLDER_ID = ""
+BOT_TOKEN = "1708976911:AAHRQXcmVQSdvC4OpkwLGWdh6vhDKV_Vjh8"
+GDRIVE_FOLDER_ID = "1sG02H8xhqp184jsXqY4UQlWFDsaMsJ_v"
 # Default folder id.
-OWNER_ID = 123455673
+OWNER_ID = 1087446551
 # Example: OWNER_ID = 619418070
 AUTHORISED_USERS = []
 # Example: AUTHORISED_USERS = [63055333, 100483029, -1003943959]
